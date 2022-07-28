020060c6983cb6117a363d09b074cbdac8a7cab7
Lucie Kretzler <>
2022-07-28 20:16:51 +0200

    Nach Löschung von überflüssigen Codezeilen (Doppelung)

--- myUI.R
+++ myUI.R
@@ -54,60 +54,6 @@ myUI <- navbarPage(
              mainPanel(
                NULL,
                uiOutput("tagebuchEintrag")
-             #   radioButtons(
-             #     "fieber",
-             #     "Fieber",
-             #     choices = list("Ich habe kein Fieber" = 0, "Ich habe Fieber" = 1),
-             #     selected = 0
-             #   ),
-             #   conditionalPanel(
-             #     condition = "input.fieber == 1",
-             #     numericInput(
-             #       "temp",
-             #       "gemessene Körpertemperatur",
-             #       37.0,
-             #       min = 35.0,
-             #       max = 42.0,
-             #       step = 0.1
-             #     ),
-             #     conditionalPanel(
-             #       condition = "input.temp > 38.5",
-             #       conditionalPanel(
-             #         condition = "input.temp > 44",
-             #         h2("Bitte messen Sie noch einmal, dieser Wert ist sehr unwahrscheinlich", style = "color:yellow"),
-             #       ),
-             #       conditionalPanel(
-             #         condition = "input.temp <= 44",
-             #         h2("Kontaktieren Sie bitte Ihren Arzt", style = "color:red")
-             #       )
-             #     ),
-             #   ),
-             #   checkboxGroupInput(
-             #     "symptome",
-             #     "Sonstige Symptome",
-             #     choices = list(
-             #       "Kopfschmerzen" = 1,
-             #       "allgemein Abgeschlagenheit" = 2,
-             #       "Appetitlosigkeit" = 3,
-             #       "Nachtschweiß" = 4,
-             #       "Muskel- oder Gelenkschmerzen" = 5
-             #     ),
-             #     selected = 0
-             #   ),
-             #   
-             #   #conditionalPanel wird nur angezeigt, wenn Medikamente hinterlegt sind
-             #   checkboxGroupInput(
-             #     "medis",
-             #     "Gestern eingenommene Medikamente",
-             #     choices = list(
-             #       "Rifampicin 600 mg (z.B. EREMFAT) 1 Tablette um 8:00" = 1,
-             #       "Rifampicin 600 mg (z.B. EREMFAT) 1 Tablette um 20:00" = 2
-             #     ),
-             #     selected = 0,
-             #     width = 500
-             #   ),
-             #   actionButton("submit","submit", icon("save")),
-             #   verbatimTextOutput("value")
              )
            )
         ),
